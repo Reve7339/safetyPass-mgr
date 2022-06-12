247998b8d6307a6f8d2ea5f108491cec55186773
Reve7339 <yerri27hinojosa.z@gmail.com>
2022-06-11 19:45:58 -0500

    config

--- app/config.py
+++ app/config.py
@@ -5,11 +5,6 @@ mysqldb = os.environ.get("MYSQSL_URL")
 
 if mysqldb is None:
     mysqldb = '127.0.0.1:33060'
-    database = 'python-flask'
-else:
-    database = 'sys'
-
-print(mysqldb, database)
 
 secretkey = secrets.token_hex(16)
 
@@ -18,5 +13,5 @@ class Config:
     SERVER_NAME = '127.0.0.1:8080'
     DEBUG = True
     ENV = 'development'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:secret@{mysqldb}/{database}'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:secret@{mysqldb}/python-flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

7d7897ced7d7914abe02e53f958b671b261725e6
Reve7339 <yerri27hinojosa.z@gmail.com>
2022-06-11 19:14:46 -0500

    some changes

--- app/config.py
+++ app/config.py
@@ -1,4 +1,15 @@
+import os
 import secrets
+
+mysqldb = os.environ.get("MYSQSL_URL")
+
+if mysqldb is None:
+    mysqldb = '127.0.0.1:33060'
+    database = 'python-flask'
+else:
+    database = 'sys'
+
+print(mysqldb, database)
 
 secretkey = secrets.token_hex(16)
 
@@ -7,5 +18,5 @@ class Config:
     SERVER_NAME = '127.0.0.1:8080'
     DEBUG = True
     ENV = 'development'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:secret@127.0.0.1:33060/python-flask'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:secret@{mysqldb}/{database}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
